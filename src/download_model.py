7f580cb96700232e643fa015c02f1ce86040b6f7
申智铭 <11059016+zhiming3150@user.noreply.gitee.com>
2026-05-30 15:19:28 +0800

    fix:路径问题

--- src/download_model.py
+++ src/download_model.py
@@ -2,7 +2,7 @@ import os
 from modelscope.hub.snapshot_download import snapshot_download
 import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.dirname(os.path.dirname(current_dir))
+project_root = os.path.dirname(current_dir) 
 sys.path.append(project_root)
 
 from src.utils.tools import get_project_root
